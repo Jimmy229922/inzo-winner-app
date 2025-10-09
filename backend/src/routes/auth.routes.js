8ad49aede8f07d389912ba3d850de484bc25ce1b
Ahmed Gamal <ahmed9ahmed779@gmail.com>
2025-10-09 17:49:40 +0200

    Ahmed#058

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,10 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controllers/auth.controller');
-const authMiddleware = require('../middleware/auth.middleware');
+const authController = require("../controllers/auth.controller");
+//const authMiddleware = require('../middleware/auth.middleware');
+const { authenticate } = require("../middleware/auth.middleware");
 
-router.post('/login', authController.login);
-router.get('/me', authMiddleware, authController.getMe);
+router.post("/login", authController.login);
+router.get("/me", authenticate, authController.getMe);
 
 module.exports = router;
-                
